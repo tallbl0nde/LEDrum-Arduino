d39c89e3bc2b648aaf9841dcfae5a82a73fa4580
tallbl0nde <40382856+tallbl0nde@users.noreply.github.com>
2019-07-11 13:45:56 +0930

    Refactor code; add 'rgb  mode'

--- LEDrum.ino
+++ LEDrum.ino
@@ -1,4 +1,5 @@
-// Defining Pins
+/* #region Definitions */
+// Pins
 #define pin_snare 6
 #define pin_high 7
 #define pin_low 10
@@ -15,36 +16,38 @@
 #define pzo_floor A4
 #define pzo_bass A3
 
-// Defining settings
+// Settings
 #define drum_bright_min 40.0
 #define drum_bright_max 700.0
 #define drum_bright_fix 200.0
 #define drum_hit_fix 80.0
 #define drum_idle_fix 110.0
-#define update_ms 15
-#define idle_ms 15000
+#define update_interval 15
+#define idle_timeout 4000
 
-// Define default colours
+// Default Colour
 #define red 0
 #define green 130
 #define blue 250
+/* #endregion */
 
 // Declaring variables
 const byte pin_out[5] = {pin_snare, pin_high, pin_low, pin_floor, pin_bass};
 const byte pin_in[5] = {pzo_snare, pzo_high, pzo_low, pzo_floor, pzo_bass};
-const int drum_hit_min[5] = {120,110,100,300,40};
-int drum_level[5] = {0,0,0,0,0};
-int piezo_level[5] = {0,0,0,0,0};
+const int drum_hit_min[5] = {120, 110, 100, 300, 40};
 
-byte rgb_r = red;
-byte rgb_g = green;
-byte rgb_b = blue;
+int drum_level[5] = {0, 0, 0, 0, 0};
+int piezo_level[5] = {0, 0, 0, 0, 0};
 
-unsigned long dec_millis = 0;
-unsigned long idle_millis = 0;
+byte rgb[3] = {0, 0, 0};
 
-int idle_mode = -1;
-byte idle_drum = 0;
+unsigned long update_ms = 0;
+unsigned long idle_ms = 0;
+
+// Depicts which mode to execute
+byte mode = 2;
+// Stores last mode (used to handle a change)
+byte mode_last = 5;
 
 void setup(){
     // Setup Pins
@@ -56,187 +59,60 @@ void setup(){
     pinMode(pin_green, OUTPUT);
     pinMode(pin_blue, OUTPUT);
 
-    // Set values
-    analogWrite(pin_red, rgb_r);
-    analogWrite(pin_green, rgb_g);
-    analogWrite(pin_blue, rgb_b);
-    for (byte i = 0; i < 5; i++){
-        analogWrite(pin_out[i], 0);
-    }
-
-    dec_millis = millis();
-    idle_millis = millis();
+    update_ms = millis();
+    idle_ms = millis();
 }
 
 void loop(){
-    // Idle
-    if ((millis() - idle_millis) > idle_ms){
-        if (idle_mode == -1){
-            // Set variables
-            rgb_r = 0;
-            rgb_g = 0;
-            rgb_b = 0;
-            analogWrite(pin_red, rgb_r);
-            analogWrite(pin_green, rgb_g);
-            analogWrite(pin_blue, rgb_b);
-            idle_mode = 0;
+    // Prepare next mode if changed
+    if (mode != mode_last){
+        switch (mode){
+            case 0:
+                setup_idle();
+                break;
+            case 1:
+                setup_drum_solid();
+                break;
+            case 2:
+                setup_drum_rgb();
+                break;
         }
-        updateIdle();
+        mode_last = mode;
+    }
 
-        // Read values
-        for (byte i = 0; i < 5; i++){
-            int tmp = analogRead(pin_in[i]);
-            piezo_level[i] = analogRead(pin_in[i]);
-        }
+    // Execute appropriate mode
+    switch (mode){
+        case 0:
+            loop_idle();
+            break;
+        case 1:
+            loop_drum_solid();
+            break;
+        case 2:
+            loop_drum_rgb();
+            break;
+    }
 
-        // If hit break out of idle mode
-        for (byte i = 0; i < 5; i++){
-            if (piezo_level[i] > drum_idle_fix){
-                idle_millis = millis();
-            }
-        }
-
-    // Not idle
-    } else {
-        if (rgb_r != red || rgb_g != green || rgb_b != blue){
-            // Reset variables
-            rgb_r = red;
-            rgb_g = green;
-            rgb_b = blue;
-            analogWrite(pin_red, rgb_r);
-            analogWrite(pin_green, rgb_g);
-            analogWrite(pin_blue, rgb_b);
-            idle_mode = -1;
-            idle_drum = 0;
-
-            for (byte i = 0; i < 5; i++){
-                analogWrite(pin_out[i], 0);
-            }
-
-            idle_millis = millis();
-        }
-
-        // Actually the drumming part
-        if ((millis() - dec_millis) > update_ms){
-            // Read values
-            for (byte i = 0; i < 5; i++){
-                int tmp = analogRead(pin_in[i]);
-                piezo_level[i] = analogRead(pin_in[i]);
-            }
-
-            // Determine which one hit
-            byte idx = 6;
-            int mx = 0;
-            for (byte i = 0; i < 5; i++){
-                if (piezo_level[i] > mx && piezo_level[i] > drum_hit_min[i]){
-                    idx = i;
-                    idle_millis = millis();
-                }
-            }
-
-            // Write brightnesses
-            if (idx != 6){
-                drum_level[idx] = drum_bright_max;
-            }
-            for (byte i = 0; i < 5; i++){
-                analogWrite(pin_out[i], byte(255 * (drum_level[i]/drum_bright_max)));
-                if (drum_level[i] >= drum_bright_min){
-                    drum_level[i] -= 20*exp(drum_level[i]/500);
-                } else {
-                    drum_level[i] = 0;
-                }
-            }
-
-            dec_millis = millis();
-        }
+    // Set idle if nothing happens after set interval
+    if (mode != 0 && (millis() - idle_ms) > idle_timeout){
+        mode = 0;
     }
 }
 
-void updateIdle(){
-    if ((millis() - dec_millis) > update_ms){
-        switch (idle_mode){
-        case 0:
-            // Off to red
-            if (rgb_r == 0){
-                digitalWrite(pin_out[idle_drum], HIGH);
-            }
-            if (rgb_r < 150){
-                rgb_r++;
-                analogWrite(pin_red, rgb_r);
-            } else {
-                idle_mode++;
-            }
-            break;
-        case 1:
-            // Red to Yellow
-            if (rgb_g < 150){
-                rgb_g++;
-                analogWrite(pin_green, rgb_g);
-            } else {
-                idle_mode++;
-            }
-            break;
-        case 2:
-            // Yellow to Green
-            if (rgb_r > 0){
-                rgb_r--;
-                analogWrite(pin_red, rgb_r);
-            } else {
-                idle_mode++;
-            }
-            break;
-        case 3:
-            // Green to Cyan
-            if (rgb_b < 150){
-                rgb_b++;
-                analogWrite(pin_blue, rgb_b);
-            } else {
-                idle_mode++;
-            }
-            break;
-        case 4:
-            // Cyan to Blue
-            if (rgb_g > 0){
-                rgb_g--;
-                analogWrite(pin_green, rgb_g);
-            } else {
-                idle_mode++;
-            }
-            break;
-        case 5:
-            // Blue to Purple
-            if (rgb_r < 150){
-                rgb_r++;
-                analogWrite(pin_red, rgb_r);
-            } else {
-                idle_mode++;
-            }
-            break;
-        case 6:
-            // Purple to Red
-            if (rgb_b > 0){
-                rgb_b--;
-                analogWrite(pin_blue, rgb_b);
-            } else {
-                idle_mode++;
-            }
-            break;
-        case 7:
-            // Red to Off
-            if (rgb_r > 0){
-                rgb_r--;
-                analogWrite(pin_red, rgb_r);
-            } else {
-                idle_mode = 0;
-                digitalWrite(pin_out[idle_drum], LOW);
-                idle_drum++;
-                if (idle_drum > 4){
-                    idle_drum = 0;
-                }
-            }
-            break;
-        }
+// Helper Functions
+// Set pwm output to match r, g, b
+void writeRGB(byte r, byte g, byte b){
+    rgb[0] = r;
+    rgb[1] = g;
+    rgb[2] = b;
+    analogWrite(pin_red, rgb[0]);
+    analogWrite(pin_green, rgb[1]);
+    analogWrite(pin_blue, rgb[2]);
+}
 
-        dec_millis = millis();
+// Turn all drum MOSFETs off
+void allOff(){
+    for (byte i = 0; i < 5; i++){
+        analogWrite(pin_out[i], 0);
     }
 }
---
+++ Modes.ino
@@ -0,0 +1,246 @@
+/* #region Idle Mode */
+byte idle_drum;
+byte idle_state;
+
+void setup_idle(){
+    writeRGB(0, 0, 0);
+    allOff();
+    idle_drum = 0;
+    idle_state = 0;
+}
+
+void loop_idle(){
+    if ((millis() - update_ms) > update_interval){
+        // Transition through all colours
+        switch (idle_state){
+            case 0:
+                digitalWrite(pin_out[idle_drum], HIGH);
+                // Off to Red
+                if (rgb[0] < 255){
+                    writeRGB(rgb[0] + 5, rgb[1], rgb[2]);
+                } else {
+                    idle_state++;
+                }
+                break;
+            case 1:
+                // Red to Yellow
+                if (rgb[1] < 255){
+                    writeRGB(rgb[0], rgb[1] + 1, rgb[2]);
+                } else {
+                    idle_state++;
+                }
+                break;
+            case 2:
+                // Yellow to Green
+                if (rgb[0] > 0){
+                    writeRGB(rgb[0] - 1, rgb[1], rgb[2]);
+                } else {
+                    idle_state++;
+                }
+                break;
+            case 3:
+                // Green to Cyan
+                if (rgb[2] < 255){
+                    writeRGB(rgb[0], rgb[1], rgb[2] + 1);
+                } else {
+                    idle_state++;
+                }
+                break;
+            case 4:
+                // Cyan to Blue
+                if (rgb[1] > 0){
+                    writeRGB(rgb[0], rgb[1] - 1, rgb[2]);
+                } else {
+                    idle_state++;
+                }
+                break;
+            case 5:
+                // Blue to Purple
+                if (rgb[0] < 255){
+                    writeRGB(rgb[0] + 1, rgb[1], rgb[2]);
+                } else {
+                    idle_state++;
+                }
+                break;
+            case 6:
+                // Purple to Red
+                if (rgb[2] > 0){
+                    writeRGB(rgb[0], rgb[1], rgb[2]- 1);
+                } else {
+                    idle_state++;
+                }
+                break;
+            case 7:
+                // Red to Off
+                if (rgb[0] > 0){
+                    writeRGB(rgb[0] - 5, rgb[1], rgb[2]);
+                } else {
+                     idle_state = 0;
+                     digitalWrite(pin_out[idle_drum], LOW);
+                     idle_drum++;
+                     if (idle_drum > 4){
+                         idle_drum = 0;
+                     }
+                }
+                break;
+        }
+        update_ms = millis();
+    }
+
+    // Read values
+    for (byte i = 0; i < 5; i++){
+        int tmp = analogRead(pin_in[i]);
+        piezo_level[i] = analogRead(pin_in[i]);
+    }
+
+    // If hit break out of idle mode
+    for (byte i = 0; i < 5; i++){
+        if (piezo_level[i] > drum_idle_fix){
+            idle_ms = millis();
+            mode = 1;
+        }
+    }
+}
+/* #endregion */
+
+/* #region Solid Colour */
+void setup_drum_solid(){
+    writeRGB(red, green, blue);
+    allOff();
+}
+
+void loop_drum_solid(){
+    if ((millis() - update_ms) >= update_interval){
+        // Read values
+        for (byte i = 0; i < 5; i++){
+            int tmp = analogRead(pin_in[i]);
+            piezo_level[i] = analogRead(pin_in[i]);
+        }
+
+        // Determine which one hit
+        byte idx = 6;
+        int mx = 0;
+        for (byte i = 0; i < 5; i++){
+            if (piezo_level[i] > mx && piezo_level[i] > drum_hit_min[i]){
+                idx = i;
+                idle_ms = millis();
+            }
+        }
+
+        // Set brightnesses
+        if (idx != 6){
+            drum_level[idx] = drum_bright_max;
+        }
+        for (byte i = 0; i < 5; i++){
+            analogWrite(pin_out[i], byte(255 * (drum_level[i] / drum_bright_max)));
+            if (drum_level[i] >= drum_bright_min){
+                drum_level[i] -= 20 * exp(drum_level[i] / 500);
+            } else {
+                drum_level[i] = 0;
+            }
+        }
+
+        update_ms = millis();
+    }
+}
+/* #endregion */
+
+/* #region RGB Shift */
+byte shift_state;
+
+void setup_drum_rgb(){
+    writeRGB(255, 0, 0);
+    allOff();
+    shift_state = 0;
+}
+
+void loop_drum_rgb(){
+    // Set colour
+    if ((millis() - update_ms) > update_interval){
+        // Transition through all colours
+        switch (shift_state){
+            case 0:
+                // Red to Yellow
+                if (rgb[1] < 255){
+                    writeRGB(rgb[0], rgb[1] + 1, rgb[2]);
+                } else {
+                    shift_state++;
+                }
+                break;
+            case 1:
+                // Yellow to Green
+                if (rgb[0] > 0){
+                    writeRGB(rgb[0] - 1, rgb[1], rgb[2]);
+                } else {
+                    shift_state++;
+                }
+                break;
+            case 2:
+                // Green to Cyan
+                if (rgb[2] < 255){
+                    writeRGB(rgb[0], rgb[1], rgb[2] + 1);
+                } else {
+                    shift_state++;
+                }
+                break;
+            case 3:
+                // Cyan to Blue
+                if (rgb[1] > 0){
+                    writeRGB(rgb[0], rgb[1] - 1, rgb[2]);
+                } else {
+                    shift_state++;
+                }
+                break;
+            case 4:
+                // Blue to Purple
+                if (rgb[0] < 255){
+                    writeRGB(rgb[0] + 1, rgb[1], rgb[2]);
+                } else {
+                    shift_state++;
+                }
+                break;
+            case 5:
+                // Purple to Red
+                if (rgb[2] > 0){
+                    writeRGB(rgb[0], rgb[1], rgb[2]- 1);
+                } else {
+                    shift_state = 0;
+                }
+                break;
+        }
+
+        // Read values
+        for (byte i = 0; i < 5; i++){
+            int tmp = analogRead(pin_in[i]);
+            piezo_level[i] = analogRead(pin_in[i]);
+        }
+
+        // Determine which one hit
+        byte idx = 6;
+        int mx = 0;
+        for (byte i = 0; i < 5; i++){
+            if (piezo_level[i] > mx && piezo_level[i] > drum_hit_min[i]){
+                idx = i;
+                idle_ms = millis();
+            }
+        }
+
+        // Set brightnesses
+        if (idx != 6){
+            drum_level[idx] = drum_bright_max;
+        }
+        for (byte i = 0; i < 5; i++){
+            analogWrite(pin_out[i], byte(255 * (drum_level[i] / drum_bright_max)));
+            if (drum_level[i] >= drum_bright_min){
+                drum_level[i] -= 20 * exp(drum_level[i] / 500);
+            } else {
+                drum_level[i] = 0;
+            }
+        }
+
+        update_ms = millis();
+    }
+
+
+}
+/* #endregion */
